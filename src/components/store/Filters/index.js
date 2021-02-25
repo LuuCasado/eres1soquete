ad6f0ef9d22bf487b9a2abbe355a9b2bb4eae0a7
unknown <casadoluciana.96@gmail.com>
2021-02-25 15:50:15 -0800

    filters start

---
+++ src/components/store/Filters/index.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react"
+import { Row, Col } from "antd"
+import { sections } from "../../../utils/constants"
+import { Checkbox } from "antd"
+
+const Filters = () => {
+  function onChange(e) {
+    console.log(`checked = ${e.target.checked}`)
+  }
+  return (
+    <Col span={6}>
+      <Row>
+        {sections.map(section => (
+          <Col span={24}>
+            <Checkbox onChange={onChange}>{section}</Checkbox>
+          </Col>
+        ))}
+      </Row>
+    </Col>
+  )
+}
+
+export default Filters
+
+/*
+      <p>filter is {counts[section] ? "on" : "off"}</p>/*     
+      <button
+            onClick={() =>
+              setCounts({
+                ...counts,
+                [section]: !counts[section] ? true : false,
+              })
+            }
+          >
+            {section}
+          </button>*/
--- src/pages/Store/index.js
+++ src/pages/Store/index.js
@@ -1,14 +1,53 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Layout from "../../components/global/Layout"
 import products from "../../../static/products.json"
 import ProductList from "../../components/store/ProductList"
+import { Row, Col } from "antd"
+import { Pagination } from "antd"
+import { Carousel } from "antd"
+import Filters from "../../components/store/Filters"
+import "./index.css"
 
-const Store = () => (
-  <Layout selectedTab={"1"}>
-    <div className="site-layout-content">
-     <ProductList products={products.products} />
-    </div>
-  </Layout>
-)
+const Store = () => {
+  /*useEffect(() => {
+    const url = "https://eres1soquete.herokuapp.com/"
+    console.log(url)
+    fetch(url)
+      // .then(response => response.json())
+      .then(data => console.log(data))
+  })*/
+
+  const [counts, setCounts] = useState({})
+
+  return (
+    <Layout selectedTab={"1"}>
+      <div className="site-layout-content">
+        <Carousel autoplay className="contentStyle">
+          <div>
+            <h3 className="contentStyle">Soquetes</h3>
+          </div>
+          <div>
+            <h3 className="contentStyle">Medias</h3>
+          </div>
+          <div>
+            <h3 className="contentStyle">Bucaneras</h3>
+          </div>
+          <div>
+            <h3 className="contentStyle">Boxers</h3>
+          </div>
+        </Carousel>
+        <Row>
+         <Filters counts={counts} setCounts={setCounts} /> 
+          <Col span={18}>
+            <ProductList products={products.products} />
+            <Pagination defaultCurrent={1} total={50} />
+          </Col>
+        </Row>
+      </div>
+    </Layout>
+  )
+}
+
+
 
 export default Store
