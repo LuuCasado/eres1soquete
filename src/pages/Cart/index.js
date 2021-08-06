914076b3f8c4dce82f4d6aa01fc1d4cf3a7f58f6
Luciana Casado <casadoluciana.96@gmail.com>
2021-08-06 11:27:26 -0300

    fixed removeFromCart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,30 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext } from "react";
 import Layout from "../../components/global/Layout";
 import ProductList from "src/components/cart/ProductList";
-import { setProductQuantity } from "src/utils/products";
+import CartContext from 'src/storage/cart';
+
 
 const Cart = () => {
-  const [products, setProducts] = useState([]);
-  const removeToCart = removedId => {
-    const removedFromCart = products.filter(({ id }) => id !== removedId);
-    setProducts(removedFromCart);
-    localStorage.setItem("cart", JSON.stringify(removedFromCart));
-  };
-  const setQuantity = (products, id, quantity) =>
-    setProducts(setProductQuantity(products, id, quantity));
-
-  useEffect(() => {
-    // eslint-disable-next-line no-undef
-    const currentCart = JSON.parse(localStorage.getItem("cart")) || [];
-    setProducts(currentCart);
-  }, []);
-
+  const { products, removeFromCart, addToCart } = useContext(CartContext);
   return (
     <Layout selectedTab={"5"}>
       <ProductList
         products={products}
-        removeToCart={removeToCart}
-        setProductQuantity={setQuantity}
+        removeFromCart={removeFromCart}
+        addToCart={addToCart}
       />
     </Layout>
   );
